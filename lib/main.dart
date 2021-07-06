ee01d72bf691b5f5d4bf8010ae5996476ee5572e
Satyam <satyam.g@capitalfloat.com>
2021-07-06 14:12:00 +0530

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,51 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome'),
-        ),
-        body: Center(
-         // child: Text(wordPair.asPascalCase),
-          child: Text("Learning Flutter"),
-        ),
-      ),
+    return const MaterialApp(
+      title: '_title',
+      home: MyStatelessWidget(),
     );
   }
 }
 
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expanded Column Sample'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.blue,
+              height: 50,
+              width: 100,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.amber,
+                width: 100,
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              height: 70,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+/*  A widget that expands a child of a Row, Column, or Flex so that the child fills the available space.
+  Using an Expanded widget makes a child of a Row, Column, or Flex expand to fill the available space along the main axis
+  (e.g., horizontally for a Row or vertically for a Column). If multiple children are expanded, the available space is divided
+  among them according to the flex factor.
+  An Expanded widget must be a descendant of a Row, Column, or Flex, and the path from the Expanded widget to its
+  enclosing Row, Column, or Flex must contain only StatelessWidgets or StatefulWidgets */
+}
